3a678ca7bf16f7289ddc1e83930316ed77897ddd
dragon <isanoguchi721@gmail.com>
2023-01-24 14:24:50 -0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { SecondPageComponent } from './commponents/second-page/second-page.component'
+import { FirstPageComponent } from './commponents/first-page/first-page.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/first', component: FirstPageComponent },
+  { path: '/second', component: SecondPageComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
