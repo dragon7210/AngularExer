284a2dffc3444f2cfaf14908dfe12e602201554a
dragon <isanoguchi721@gmail.com>
2023-01-24 20:18:37 -0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
-import { SecondPageComponent } from './commponents/second-page/second-page.component'
-import { FirstPageComponent } from './commponents/first-page/first-page.component'
 
 const routes: Routes = []
 
