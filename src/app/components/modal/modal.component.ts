860992160b9cd43754b22d5a7a8c55ac2502e9f9
dragon <isanoguchi721@gmail.com>
2023-02-08 10:27:54 -0500

    fix

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { Router, ActivatedRoute, ParamMap } from '@angular/router'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-modal',
