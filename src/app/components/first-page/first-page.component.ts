867ab568bdf859fe493b88817819f8e736ad2c99
dragon <isanoguchi721@gmail.com>
2023-02-10 07:06:00 -0500

    fix

--- src/app/components/first-page/first-page.component.ts
+++ src/app/components/first-page/first-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
 
 @Component({
@@ -7,10 +8,30 @@ import { Router } from '@angular/router'
   styleUrls: ['./first-page.component.scss'],
 })
 export class FirstPageComponent implements OnInit {
-  constructor(private router: Router) {}
+  addRecord = false
+  tableForm!: FormGroup
 
-  ngOnInit() {}
-  openModal() {
-    this.router.navigate(['/modal/3'], { queryParams: { order: 'popular' } })
+  constructor(private router: Router, private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.tableForm = this.formBuilder.group({
+      addForm: this.formBuilder.array([]),
+      editForm: this.formBuilder.array([]),
+    })
+    const control1 = this.tableForm.controls['addForm'] as FormArray
+    for (let i = 0; i < this.adultCount; i++) {
+      control1.push(this.adultForm())
+    }
+    const control2 = this.tableForm.controls['editForm'] as FormArray
+    for (let i = 0; i < this.childCount; i++) {
+      control2.push(this.childForm())
+    }
+    addForm: this.formBuilder.array([])
+    this.createForm()
   }
+  add() {
+    this.addRecord = true
+    console.log(this.addRecord)
+  }
+  createForm() {}
 }
