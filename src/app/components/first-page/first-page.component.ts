60111d367677a7c29586c0707208a7f9a8ef11ab
dragon <isanoguchi721@gmail.com>
2023-02-10 19:17:33 -0500

    fix

--- src/app/components/first-page/first-page.component.ts
+++ src/app/components/first-page/first-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Inject, OnInit } from '@angular/core'
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
 
@@ -11,27 +11,29 @@ export class FirstPageComponent implements OnInit {
   addRecord = false
   tableForm!: FormGroup
 
-  constructor(private router: Router, private formBuilder: FormBuilder) {}
-
-  ngOnInit() {
-    this.tableForm = this.formBuilder.group({
-      addForm: this.formBuilder.array([]),
-      editForm: this.formBuilder.array([]),
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    @Inject(FormBuilder) fb: FormBuilder,
+  ) {
+    this.tableForm = fb.group({
+      addForm: fb.group({
+        title: ['', Validators.required],
+        content: ['', Validators.required],
+      }),
+      editFrom: fb.group({
+        title: ['', Validators.required],
+        content: ['', Validators.required],
+      }),
     })
-    const control1 = this.tableForm.controls['addForm'] as FormArray
-    for (let i = 0; i < this.adultCount; i++) {
-      control1.push(this.adultForm())
-    }
-    const control2 = this.tableForm.controls['editForm'] as FormArray
-    for (let i = 0; i < this.childCount; i++) {
-      control2.push(this.childForm())
-    }
-    addForm: this.formBuilder.array([])
-    this.createForm()
   }
+
+  ngOnInit() {}
+
   add() {
     this.addRecord = true
-    console.log(this.addRecord)
   }
-  createForm() {}
+  put() {
+    console.log(this.tableForm)
+  }
 }
