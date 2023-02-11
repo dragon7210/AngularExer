b1f650d27db533dbda1d8782420bc90988058309
dragon <isanoguchi721@gmail.com>
2023-02-11 07:36:12 -0500

    fix

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Router } from '@angular/router'
-// import 'rxjs/add/operator/filter'
-// import { filter } from 'rxjs'
 
 @Component({
   selector: 'app-modal',
